f387e090fa79bc7638815bb2f25651cf936b19b0
k6alvarez <k6alvarez@gmail.com>
2016-09-17 00:42:02 -0400

    Add new one page theme, forgot to update gitignore

---
+++ wp-content/themes/alvarez-one-page/footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Alvarez_One_Page
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'alvarez-one-page' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'alvarez-one-page' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'alvarez-one-page' ), 'alvarez-one-page', '<a href="http://kennyalvarez.com" rel="designer">Kenny Alvarez</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
